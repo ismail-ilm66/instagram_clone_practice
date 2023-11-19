08b7223bb962f0799f149ed2fdd0e56902510d56
ILM._. <ilm68265@gmail.com>
2023-11-19 19:27:44 +0500

    Added Some Functionality to home screen

--- lib/responsive/mobile_screen.dart
+++ lib/responsive/mobile_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone_practice/models/user.dart' as model;
 import 'package:instagram_clone_practice/providers/user_provider.dart';
+import 'package:instagram_clone_practice/utilities/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreen extends StatefulWidget {
@@ -12,24 +14,89 @@ class MobileScreen extends StatefulWidget {
 
 class _MobileScreenState extends State<MobileScreen> {
   model.User? user;
-
-  void getUserData() {
+  int _page = 0;
+  PageController? _pageController;
+  void getUserData() async {
     UserProvider userProvider = Provider.of(context, listen: false);
-    userProvider.refreshUser();
+    await userProvider.refreshUser();
     user = userProvider.getUser;
+    setState(() {});
+  }
+
+  void jumpToOther_page(int p) {
+    _pageController!.jumpToPage(p);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _pageController = PageController();
     getUserData();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _pageController!.dispose();
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(user == null ? 'Mobile Screen' : user!.username),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search,
+                color: _page == 1 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_rounded,
+                color: _page == 2 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_sharp,
+                color: _page == 3 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person,
+                color: _page == 4 ? primaryColor : secondaryColor),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: jumpToOther_page,
+      ),
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        onPageChanged: (value) {
+          setState(() {
+            _page = value;
+          });
+        },
+        children: [
+          Center(child: const Text('Home')),
+          Center(child: const Text('Search')),
+          Center(child: const Text('Add')),
+          Center(child: const Text('Favorite')),
+          Center(child: const Text('Profile')),
+        ],
       ),
     );
   }
