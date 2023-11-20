42b602e694b29af968a23631e176eb11b0ddce20
unknown <ilm68265@gmail.com>
2023-11-20 22:18:38 +0500

    Added Post Model

---
+++ lib/models/post.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Post {
+  final String description;
+  final String uid;
+  final String profilePhoto;
+  final String username;
+  final String postId;
+  final String postUrl;
+  final List datePublished;
+  final int likes;
+
+  Post({
+    required this.description,
+    required this.uid,
+    required this.profilePhoto,
+    required this.username,
+    required this.postId,
+    required this.postUrl,
+    required this.datePublished,
+    required this.likes,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'description': description,
+        'uid': uid,
+        'profilePhoto': profilePhoto,
+        'username': username,
+        'postId': postId,
+        'postUrl': postUrl,
+        'datePublished': datePublished,
+        'likes': likes,
+      };
+  static Post fromSnapshot(DocumentSnapshot snapshot) {
+    Map<String, dynamic> x = snapshot.data() as Map<String, dynamic>;
+    return Post(
+      description: x['description'],
+      uid: x['uid'],
+      profilePhoto: x['profilePhoto'],
+      username: x['username'],
+      postId: x['postId'],
+      postUrl: x['postUrl'],
+      datePublished: x['datePublished'],
+      likes: x['likes'],
+    );
+  }
+}
