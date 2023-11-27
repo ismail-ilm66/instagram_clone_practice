7a4888ae2741671208823066d2f17f94dcd8c917
unknown <ilm68265@gmail.com>
2023-11-27 22:56:06 +0500

    A

--- lib/responsive/feed_screen.dart
+++ lib/responsive/feed_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone_practice/utilities/colors.dart';
+
 import 'package:instagram_clone_practice/widgets/post_card.dart';
 
 class Feeds extends StatefulWidget {
@@ -28,7 +30,21 @@ class _FeedsState extends State<Feeds> {
           )
         ],
       ),
-      body: PostCard(),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                return PostCard();
+              },
+            );
+          }),
     );
   }
 }
